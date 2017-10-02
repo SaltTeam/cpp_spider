897d5772a178f45432c071ccf6c49c8152effec7
HugoSoszynski <hugo.soszynski@outlook.com>
2017-10-02 10:52:01 +0200

    The network start to become available. Need the classes for other devs.

--- sources/Network/BoostNetworkSession.cpp
+++ sources/Network/BoostNetworkSession.cpp
@@ -22,21 +22,32 @@ spider::BoostNetworkSession::BoostNetworkSession(std::string const& host,
   _context = new boost::asio::ssl::context(boost::asio::ssl::context::tlsv12);
   _context->load_verify_file(CLIENT_CERT);
 
-  _socket = new boost_ssl_socket(_ios, _context);
+  _socket = new boost_ssl_socket(*_ios, *_context);
   _socket->set_verify_mode(boost::asio::ssl::verify_peer);
 }
 
 spider::BoostNetworkSession::BoostNetworkSession(unsigned short port)
   : _host(), _port(port)
 {
+  _ios = new boost::asio::io_service();
 
+  boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), port);
+  _acceptor = new boost::asio::ip::tcp::acceptor(*_ios, endpoint);
+
+  _context = new boost::asio::ssl::context(boost::asio::ssl::context::tlsv12);
+  _context->set_options(boost::asio::ssl::context::default_workarounds
+        | boost::asio::ssl::context::no_sslv2
+        | boost::asio::ssl::context::no_sslv3
+        | boost::asio::ssl::context::single_dh_use);
+  _context->set_password_callback(spider::BoostNetworkSession::get_ssl_passwd());
+  _context->use_certificate_chain_file(SERVER_CERT);
+  _context->use_private_key_file(SERVER_KEY);
 }
 
-spider::BoostNetworkSession::BoostNetworkSession(boost::asio::io_service& io_service,
-boost::asio::ssl::context& context)
+spider::BoostNetworkSession::BoostNetworkSession(std::unique_ptr<boost_ssl_socket>& socket)
   : _host(), _port(0)
 {
-
+  _socket = std::move(socket);
 }
 
 spider::BoostNetworkSession::~BoostNetworkSession()
@@ -44,42 +55,41 @@ spider::BoostNetworkSession::~BoostNetworkSession()
 
 }
 
-void spider::BoostNetworkSession::setConnectCallback()
+template <typename ConnectHandler>
+void spider::BoostNetworkSession::Connect(ConnectHandler hdl)
+{
+  this->_socket->lowest_layer().async_connect(
+    boost::asio::ip::tcp::endpoint(
+      boost::asio::ip::address::from_string(this->_host),
+      this->_port),
+    hdl);
+}
+
+template <typename SendHandler>
+void spider::BoostNetworkSession::Send(std::string const& msg, SendHandler hdl)
 {
 
 }
 
-void spider::BoostNetworkSession::setSendCallback()
+template <typename RecvHandler>
+void spider::BoostNetworkSession::Recv(RecvHandler hdl)
 {
 
 }
 
-void spider::BoostNetworkSession::setRecvCallback()
+template<typename AcceptHandler>
+spider::BoostNetworkSession& spider::BoostNetworkSession::Accept(AcceptHandler hdl)
+{
+  std::unique_ptr<boost_ssl_socket> sock(new boost_ssl_socket(_ios, _context));
+  _acceptor->async_accept(sock->lowest_layer(), hdl);
+}
+
+void spider::BoostNetworkSession::Select()
 {
 
 }
 
-void spider::BoostNetworkSession::setAcceptCallback()
+std::string spider::BoostNetworkSession::get_ssl_passwd() const
 {
-
-}
-
-void spider::BoostNetworkSession::Connect()
-{
-
-}
-
-void spider::BoostNetworkSession::Send()
-{
-
-}
-
-void spider::BoostNetworkSession::Recv()
-{
-
-}
-
-void spider::BoostNetworkSession::Accept()
-{
-
+  return SERVER_PASSWD;
 }
