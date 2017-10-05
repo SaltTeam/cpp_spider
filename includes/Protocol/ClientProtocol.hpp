71d097cbc4c95b7f29bb1d689b287875b02e6d48
pillon_m <maxime.pillon@epitech.eu>
2017-10-05 14:16:08 +0200

    edit protocol, issue : need to change serializer to singleton

---
+++ includes/Protocol/ClientProtocol.hpp
@@ -0,0 +1,29 @@
+/*
+** ClientProtocol.hpp for cpp_spider in /home/god/TEK3/C++/cpp_spider/includes/Protocol/ClientProtocol.hpp
+**
+** Made by Maxime PILLON
+** Login   <maxime.pillon@epitech.eu>
+**
+** Started on  Thu Oct 05 11:40:15 2017 Maxime PILLON
+** Last update Thu Oct 05 11:40:15 2017 Maxime PILLON
+*/
+
+#pragma once
+
+#include "Network/ClientNetwork.hpp"
+#include "IProtocol.hpp"
+#include "Serializer.hpp"
+
+namespace spider
+{
+  class ClientProtocol : public IProtocol
+  {
+  protected:
+    ClientNetwork	net;
+    Serializer		serializer;
+
+  public:
+    ClientProtocol();
+    ~ClientProtocol();
+  };
+}
---
+++ sources/Protocol/ClientProtocol.cpp
@@ -0,0 +1,10 @@
+/*
+** ClientProtocol.cpp for cpp_spider in /home/god/TEK3/C++/cpp_spider/sources/Protocol/ClientProtocol.cpp
+**
+** Made by Maxime PILLON
+** Login   <maxime.pillon@epitech.eu>
+**
+** Started on  Thu Oct 05 11:45:58 2017 Maxime PILLON
+** Last update Thu Oct 05 11:45:58 2017 Maxime PILLON
+*/
+
