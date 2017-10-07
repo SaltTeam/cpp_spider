aad68c25b215ae59de5d75836a2f9bd6d4bd87e6
HugoSoszynski <hugo.soszynski@outlook.com>
2017-10-07 16:17:25 +0200

    New certificates for group compatibility. Adding debug logs.

--- sources/Network/ServerNetwork.cpp
+++ sources/Network/ServerNetwork.cpp
@@ -9,6 +9,7 @@
 */
 
 #include <boost/bind.hpp>
+#include <iostream>
 #include "Protocol/Buffer.hpp"
 #include "Network/ServerNetwork.hpp"
 
@@ -16,14 +17,16 @@ spider::ServerNetwork::ServerNetwork(unsigned short port)
   : _ios(), _context(boost::asio::ssl::context::tlsv12),
     _acceptor(_ios, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
 {
+  std::clog << "Creating server" << std::endl;
   _context.set_options(
     boost::asio::ssl::context::default_workarounds
     | boost::asio::ssl::context::no_sslv2
     | boost::asio::ssl::context::single_dh_use);
   _context.set_password_callback(boost::bind(&ServerNetwork::get_password, this));
-  _context.use_certificate_chain_file("resources/pki_tmp/server.crt");
-  _context.use_private_key_file("resources/pki_tmp/server.key",
+  _context.use_certificate_chain_file("resources/cert/cert.pem");
+  _context.use_private_key_file("resources/cert/key.pem",
 				boost::asio::ssl::context::pem);
+  _context.use_tmp_dh_file("resources/cert/dHParam.pem");
 
   this->accept();
 }
@@ -33,6 +36,7 @@ spider::ServerNetwork::~ServerNetwork() = default;
 
 void spider::ServerNetwork::accept()
 {
+  std::clog << "Accepting..." << std::endl;
   NetworkSession* new_session = new NetworkSession(_ios, _context);
   _acceptor.async_accept(new_session->socket(),
 			 boost::bind(&ServerNetwork::handleAccept,
@@ -43,16 +47,20 @@ void spider::ServerNetwork::accept()
 void spider::ServerNetwork::handleAccept(NetworkSession* session,
 					 boost::system::error_code const& error)
 {
+  std::clog << "HAccepting..." << std::endl;
   if (!error)
     session->start();
   else
+  {
+    std::clog << "Accept error: " << error.message() << std::endl;
     delete session;
+  }
   this->accept();
 }
 
 std::string spider::ServerNetwork::get_password() const
 {
-  return "test_pem_password";
+  return "toto";
 }
 
 spider::NetworkSession::NetworkSession(boost::asio::io_service& io_service,
@@ -71,6 +79,7 @@ void spider::NetworkSession::send(std::string const& msg)
 
 void spider::NetworkSession::start()
 {
+  std::clog << "Startnig session..." << std::endl;
   _socket.async_handshake(
     boost::asio::ssl::stream_base::server,
     boost::bind(&NetworkSession::handleHandshake,
@@ -83,6 +92,7 @@ spider::NetworkSession::handleHandshake(boost::system::error_code const& error)
 {
   if (!error)
   {
+    std::clog << "Session handshake..." << std::endl;
     _socket.async_read_some(
       boost::asio::buffer(_light_buf, NET_BUFFER_LEN),
       boost::bind(&NetworkSession::handleRead,
@@ -99,6 +109,9 @@ void spider::NetworkSession::handleRead(boost::system::error_code const& error,
 {
   if (!error)
   {
+    std::clog << "Reading..." << std::endl;
+    std::clog.write(_light_buf, bytes_transferred);
+    std::clog << std::endl;
     Buffer &buf = Buffer::BufferInstance();
     std::string str(_light_buf);
     str.resize(bytes_transferred);
