322e4c7586a3e89e0c183855aa5e51169a5958ba
Maxence Delacroix <delacr_a@localhost.localdomain>
2017-10-07 15:50:45 +0200

    debug regex (prise en compte des espaces, virgules, points, points virgules)

--- sources/Protocol/ServerProtocol.cpp
+++ sources/Protocol/ServerProtocol.cpp
@@ -68,7 +68,7 @@ void spider::ServerProtocol::run()
 {
   Buffer &buf = Buffer::BufferInstance();
   std::string &str = buf.getBuf();
-  std::regex reg = std::regex("\\{(?:(?:\\s*\"\\w+\": \"\\w+\",{0,1}\\s*)+\"data\": \\{(?:\\s*\"\\w+\": \"\\w+\",{0,1}\\s*)+\\}(?:,{0}|,{1}(?:\\s*\"\\w+\": \"\\w+\",{0,1}\\s*)+)|(?:\\s*\"\\w+\": \"\\w+\",{0,1}\\s*)+)\\}");
+  std::regex reg = std::regex("\\{(?:(?:\\s*\"[\\w\\s.;,_]+\": \"[\\w\\s.;,_]+\",{0,1}\\s*)+\"data\": \\{(?:\\s*\"[\\w\\s.;,_]+\": \"[\\w\\s.;,_]+\",{0,1}\\s*)+\\}(?:,{0}|,{1}(?:\\s*\"[\\w\\s.;,_]+\": \"[\\w\\s.;,_]+\",{0,1}\\s*)+)|(?:\\s*\"[\\w\\s.;,_]+\": \"[\\w\\s.;,_]+\",{0,1}\\s*)+)\\}");
 
   for (auto it = std::sregex_iterator(str.begin(), str.end(), reg);
        it != std::sregex_iterator(); ++it)
