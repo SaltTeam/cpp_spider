8a6b9918e4afbe45799dabe0a6329474dd5629ad
loens_g <gregoire.loens@epitech.eu>
2017-10-08 19:05:40 +0200

    doxygen sur les threads

--- includes/Thread/IThread.hpp
+++ includes/Thread/IThread.hpp
@@ -8,16 +8,23 @@
 #include <memory>
 #include "Protocol/IProtocol.hpp"
 
+/// \namespace spider
 namespace spider {
 
+    /// \interface IThread
     class IThread {
     public:
+
+        /// \brief Create a new thread for the protocol run function
         virtual void createThread(std::unique_ptr<IProtocol> &protocl) = 0;
 
-      virtual void createNetThread() = 0;
+        /// \brief Create thread for net run service
+        virtual void createNetThread() = 0;
 
-      virtual void createNetServThread() = 0;
+        /// \brief Create thread for server net run service
+        virtual void createNetServThread() = 0;
 
+        /// \brief Join thread
         virtual void threadJoin() = 0;
     };
 };
