57c00e2e8c2bbc50bf1f7c129eddf7b2e67cad89
Maxence Delacroix <delacr_a@localhost.localdomain>
2017-10-08 19:25:10 +0200

    doxygen

--- includes/Protocol/Serializer.hpp
+++ includes/Protocol/Serializer.hpp
@@ -1,9 +1,9 @@
 //
 // Serializer.hpp for cpp_spider in /home/delacr_a/rendu/CPP/cpp_spider
-// 
+//
 // Made by Maxence Delacroix
 // Login   <delacr_a@epitech.net>
-// 
+//
 // Started on  Fri Oct  6 15:54:01 2017 Maxence Delacroix
 // Last update Fri Oct  6 15:54:02 2017 Maxence Delacroix
 //
@@ -15,8 +15,10 @@
 #include <map>
 #include "dataStructure.hpp"
 
+/// \namespace spider
 namespace spider
 {
+  /// \class Serializer
   class Serializer
   {
   private:
@@ -25,15 +27,53 @@ namespace spider
     std::map<KEYTYPE, std::string> keytype_string;
 
   public:
+    /// \brief Function to get the Serializer or create it if he hasnn't been created yet.
+    /// \return Returns the Serializer object.
     static Serializer &getSerializer();
+
+    /// \brief Creates a ptree with the infos contained in the data structure (message).
+    /// \param A t_message structure containing all the infos that needs to be serialized.
+    /// \return Returns a ptree containing the infos of the structure that was given as parameter.
     boost::property_tree::ptree serialize(t_message const &data) const;
+
+    /// \brief Creates a ptree with the infos contained in the data structure (register).
+    /// \param A t_register structure containing all the infos that needs to be serialized.
+    /// \return Returns a ptree containing the infos of the structure that was given as parameter.
     boost::property_tree::ptree serialize(t_register const &data) const;
+
+    /// \brief Creates a ptree with the infos contained in the data structure (mouse).
+    /// \param A t_mouse structure containing all the infos that needs to be serialized.
+    /// \return Returns a ptree containing the infos of the structure that was given as parameter.
     boost::property_tree::ptree serialize(t_mouse const &data) const;
+
+    /// \brief Creates a ptree with the infos contained in the data structure (command).
+    /// \param A t_command structure containing all the infos that needs to be serialized.
+    /// \return Returns a ptree containing the infos of the structure that was given as parameter.
     boost::property_tree::ptree serialize(t_command const &data) const;
+
+    /// \brief Creates a t_unserialized structure that contains one structure with the infos that are contained in ptree (message, register, mouse or command).
+    /// \param A ptree containing all the infos that needs to be unserialized.
+    /// \return Returns a t_unserialized structure containing one structure with the infos that was contained in ptree (message, register, mouse or command).
     t_unserialized unserialize(boost::property_tree::ptree const &pt);
+
+    /// \brief This function creates a ptree from a json formatted string.
+    /// \param A json formatted string.
+    /// \return Returns a ptree which contain the infos contained in the json formatted string
     boost::property_tree::ptree get_ptree_from_string(std::string const &json) const;
+
+    /// \brief This function creates a json formatted string from a ptree.
+    /// \param A ptree that contains the info you need to retrieve.
+    /// \return Returns a string which contain the infos contained in the ptree in json format
     std::string get_string_from_ptree(boost::property_tree::ptree const &pt) const;
+
+    /// \brief This function is used to get the name of the keytype in a string.
+    /// \param A keytype you want to retrieve the name.
+    /// \return Returns the string that correspond to name of the keytype.
     std::string get_string_from_keytype(KEYTYPE const &key) const;
+
+    /// \brief This function is used to get the keytype value from its name.
+    /// \param A string correponding to the name of the keytype.
+    /// \return Returns the keytype value that correspond to the keytype name passed in parameter.
     KEYTYPE get_keytype_from_string(std::string const &str) const;
 
   private:
--- includes/dataStructure.hpp
+++ includes/dataStructure.hpp
@@ -7,6 +7,7 @@
 
 # include <string>
 
+/// \enum KEYTYPE
 enum class	KEYTYPE
 {
     KNONE = 0,
@@ -17,43 +18,48 @@ enum class	KEYTYPE
     MC
 };
 
+/// \struct t_message
 struct		        t_message
 {
-    std::string 	timestamp;
-    std::string	    process;
-    std::string	    data;
-    std::string     type;
+    std::string 	timestamp; //!< The timestamp at which the info was collected.
+    std::string		process; //!< The process on which the info was collected.
+    std::string		data; //!< The info that was collected (letters).
+    std::string		type; //!< The type of the structure (KEYSTROKE).
 };
 
-struct              t_mouse
+/// \struct t_mouse
+struct			t_mouse
 {
-    std::string     timestamp;
-    std::string	    process;
-    KEYTYPE         keytype;
-    unsigned int    x;
-    unsigned int    y;
-    std::string     type;
+    std::string		timestamp; //!< The timestamp at which the info was collected.
+    std::string		process; //!< The process on which the info was collected.
+    KEYTYPE		keytype; //!<  Type of click.
+    unsigned int	x; //!< The x position of the mouse when the info was collected.
+    unsigned int	y; //!< The y position of the mouse when the info was collected.
+    std::string		type; //!<  The type of the structure (MOUSE).
 };
 
-struct		    t_register
+/// \struct t_register
+struct			t_register
 {
-    std::string os;
-    std::string	mac;
-    std::string	antivirus;
+    std::string		os; //!< The OS the user is running.
+    std::string		mac; //!< The MAC address of the user.
+    std::string		antivirus; //!< The antivirus used by the user.
 };
 
-struct          t_command
+/// \struct t_command
+struct			t_command
 {
-    std::string type;
-    std::string data;
+    std::string		type; //!< The name of the command.
+    std::string		data; //!< Data that may be used by the command.
 };
 
-struct          t_unserialized
+/// \struct t_unserialized
+struct		t_unserialized
 {
-    t_message   *message;
-    t_register  *register_;
-    t_mouse     *mouse;
-    t_command   *command;
+    t_message	*message; //!< The message infos collected after unserializing data of a ptree.
+    t_register	*register_; //!< The register infos collected after unserializing data of a ptree.
+    t_mouse	*mouse; //!< The mouse infos collected after unserializing data of a ptree.
+    t_command	*command; //!< The command infos collected after unserializing data of a ptree.
 };
 
 #endif //CPP_SPIDER_DATASTRUCTURE_H
